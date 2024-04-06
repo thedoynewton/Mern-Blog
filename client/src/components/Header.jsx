f7ab027a6bf532805af8519494485cc208595d9a
dadoy <106452209+thed0y@users.noreply.github.com>
2024-04-07 02:44:01 +0800

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,7 +29,7 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='purpleToBlue'outline>
             Sign in
           </Button>
         </Link>
